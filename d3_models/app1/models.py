decce35e535306922e0c63523c88823b031edf7f
Mahesh Jadhav <omkar28399@gmail.com>
2024-03-01 15:49:22 +0530

    Added code to use models and how to add them to database

---
+++ d3_models/app1/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length = 20)
+    division = models.CharField(max_length=10)
+    about = models.TextField()
+    roll = models.IntegerField()
+
+class Teacher(models.Model):
+    teacher_code = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length = 20)
+    subject = models.CharField(max_length = 10)
+    
+
+class Subject(models.Model):
+    name = models.CharField(max_length = 10)
+    code = models.IntegerField()
