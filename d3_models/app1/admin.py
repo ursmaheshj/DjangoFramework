e80c64dbc487961860aa54f364db894f6315b91f
Mahesh Jadhav <omkar28399@gmail.com>
2024-03-01 18:49:28 +0530

    Added decorater instead of site.register in admin.py

--- d3_models/app1/admin.py
+++ d3_models/app1/admin.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 from .models import Student,Teacher,Subject
 
 # Register your models here.
+@admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
     list_display = ['name','division','about']
 
-admin.site.register(Student,StudentAdmin)
+# class StudentAdmin(admin.ModelAdmin):
+#     list_display = ['name','division','about']
+# admin.site.register(Student,StudentAdmin)
+
 admin.site.register(Teacher)
 admin.site.register(Subject)
