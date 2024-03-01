9838ed7cae0ef30c26784e2e2e512a244e5224f7
Mahesh Jadhav <omkar28399@gmail.com>
2024-03-01 18:38:13 +0530

    Used ModelAdmin to register models in admin site to view fields as table data

--- d3_models/app1/admin.py
+++ d3_models/app1/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from .models import Student,Teacher,Subject
 
 # Register your models here.
-admin.site.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['name','division','about']
+
+admin.site.register(Student,StudentAdmin)
 admin.site.register(Teacher)
 admin.site.register(Subject)
