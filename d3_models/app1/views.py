786ab93942510acd33896865c12d834a3cb5fae7
Mahesh Jadhav <omkar28399@gmail.com>
2024-03-01 17:26:26 +0530

    Added code to retrieve data from database using model queries

--- d3_models/app1/views.py
+++ d3_models/app1/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from app1.models import Student
+
 # Create your views here.
+
+def home(request):
+    students = Student.objects.all()
+    student = Student.objects.first()
+    context = {
+        'students':students,
+        'student':student
+    }
+    return render(request,'app1/home.html',context)
--- d3_models/d3_models/settings.py
+++ d3_models/d3_models/settings.py
@@ -52,10 +52,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'd3_models.urls'
 
+TEMPLATES_DIR= BASE_DIR / 'templates'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
