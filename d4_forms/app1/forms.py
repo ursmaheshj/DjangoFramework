5b0516bae2a534faeb49f97a2cc275c2987be7a6
Mahesh Jadhav <omkar28399@gmail.com>
2024-03-03 01:08:29 +0530

    How to render form fields manually in templates

--- d4_forms/app1/forms.py
+++ d4_forms/app1/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class StudentForm(forms.Form):
-    name = forms.CharField(required=False)
+    name = forms.CharField(required=False,help_text="Demo help")
     div = forms.CharField()
     code = forms.IntegerField()
     about = forms.CharField(widget=forms.Textarea)
