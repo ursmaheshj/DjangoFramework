98b0075ecc9f26a8f45a4daee0b47c4297b320a8
Mahesh Jadhav <omkar28399@gmail.com>
2024-03-03 00:23:11 +0530

    Configured auto_id, label_suffix, initial

--- d4_forms/app1/views.py
+++ d4_forms/app1/views.py
@@ -4,7 +4,8 @@ from app1.forms import StudentForm
 
 # Create your views here.
 def home(request):
-    studentform = StudentForm
+    initial_val = {'name':"Mahesh",'div':'A'}
+    studentform = StudentForm(auto_id='Mahesh_%s',label_suffix="=",initial=initial_val)
     context = {
         'studentform':studentform
     }
