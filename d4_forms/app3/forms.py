ae378ae560e5e61ad4460953a20b54ef96b4a93c
Mahesh Jadhav <omkar28399@gmail.com>
2024-03-10 13:52:15 +0530

    Added custom validator check_email_domain in d4_forms/app3

--- d4_forms/app3/forms.py
+++ d4_forms/app3/forms.py
@@ -1,7 +1,11 @@
 from django.core import validators
 from django import forms
 
+def check_email_domain(value):
+    if value[-10:]!='@gmail.com':
+        raise forms.ValidationError('Incorrect Domain')
+
 class StudentForm(forms.Form):
     name = forms.CharField(validators=[validators.MaxLengthValidator(4)])
-    email = forms.EmailField()
+    email = forms.EmailField(validators=[check_email_domain])
     
