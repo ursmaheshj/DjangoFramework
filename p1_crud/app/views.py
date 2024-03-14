e2d1a9795af51ce920c48d9f11cc4feca73392c2
Mahesh Jadhav <omkar28399@gmail.com>
2024-03-14 17:02:39 +0530

    Added basic templates and view function

--- p1_crud/app/views.py
+++ p1_crud/app/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def addshowview(request):
+    return render(request,'app/addandshow.html')
--- p1_crud/p1_crud/urls.py
+++ p1_crud/p1_crud/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from app.views import addshowview
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', addshowview, name='addshow')
 ]
