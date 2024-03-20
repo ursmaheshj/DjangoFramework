70ec88377659837fbea8c9c8070f5313d6fc4be2
Mahesh Jadhav <omkar28399@gmail.com>
2024-03-20 10:58:56 +0530

    Changing tag name by overriding constants in settings.py

--- d6_messages/d6_messages/settings.py
+++ d6_messages/d6_messages/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+from django.contrib.messages import constants
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -123,3 +123,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Message Framework configurations
+MESSAGE_TAGS = {
+    constants.ERROR:'Danger',
+    constants.SUCCESS:'Good'
+}
