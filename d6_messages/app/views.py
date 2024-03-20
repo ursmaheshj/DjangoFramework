01df665237c9c47b5a2d7c4422ad0ee41f3191c6
Mahesh Jadhav <omkar28399@gmail.com>
2024-03-20 10:53:52 +0530

    Added all message tag types

--- d6_messages/app/views.py
+++ d6_messages/app/views.py
@@ -4,6 +4,16 @@ from app.models import Student
 from app.forms import StudentForm
 
 # Create your views here.
+def getmessages(request):
+
+    messages.debug(request,'This is debug') #This wont be visible as default level is 20
+    messages.info(request,'This is info')
+    messages.success(request,'This is success')
+    messages.warning(request,'This is warning')
+    messages.error(request,'This is error')
+
+    return render(request,'app/messages.html')
+
 def registration(request):
     if request.method == 'POST':
         studentform = StudentForm(request.POST)
--- d6_messages/d6_messages/urls.py
+++ d6_messages/d6_messages/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app.views import registration
+from app.views import registration,getmessages
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',registration)
+    path('',registration),
+    path('getmessages',getmessages)
 ]
