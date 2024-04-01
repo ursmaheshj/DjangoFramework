fdc6eab6ee52d4d9da9fa60b44b4e9d5fa7f37e4
Mahesh Jadhav <omkar28399@gmail.com>
2024-04-01 22:44:39 +0530

    Message framework added for more convenience

--- d7_authenticationsytem/app1/views.py
+++ d7_authenticationsytem/app1/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 # from django.contrib.auth.forms import UserCreationForm
 from app1.forms import MyUserCreationForm as UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
 def sign_up(request):
@@ -9,4 +10,5 @@ def sign_up(request):
         fm = UserCreationForm(request.POST)
         if fm.is_valid():
             fm.save()
+            messages.success(request,'User has been created!')
     return render(request,'app1/registration.html',{'form':fm})
