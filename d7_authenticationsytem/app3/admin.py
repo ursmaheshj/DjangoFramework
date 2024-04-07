873cf6f4613777979b49eb3eabff578104d5c172
Mahesh Jadhav <omkar28399@gmail.com>
2024-04-07 18:31:00 +0530

    Added blog model

--- d7_authenticationsytem/app3/admin.py
+++ d7_authenticationsytem/app3/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from app3.models import Blog
 
 # Register your models here.
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ['title','desc']
+        
--- d7_authenticationsytem/app3/views.py
+++ d7_authenticationsytem/app3/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponseRedirect
 
 # Create your views here.
+def Dashboard(request):
+    if request.user.is_authenticated:
+        pass    
+    else:
+        return HttpResponseRedirect('/login/')
