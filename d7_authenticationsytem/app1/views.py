21ccb31024f1a727793d3b7488dc4648b272ff5c
Mahesh Jadhav <omkar28399@gmail.com>
2024-04-08 09:51:58 +0530

    Updated code to add group to user while signup

--- d7_authenticationsytem/app1/views.py
+++ d7_authenticationsytem/app1/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 # from django.contrib.auth.forms import UserCreationForm
 from app1.forms import MyUserCreationForm
 from django.contrib import messages
+from django.contrib.auth.models import Group
 
 # Create your views here.
 def sign_up(request):
@@ -9,6 +10,8 @@ def sign_up(request):
     if request.method == 'POST':
         fm = MyUserCreationForm(request.POST)
         if fm.is_valid():
-            fm.save()
+            user = fm.save()
+            group = Group.objects.get(name='Editor')
+            user.groups.add(group)
             messages.success(request,'User has been created!')
     return render(request,'app1/registration.html',{'form':fm})
