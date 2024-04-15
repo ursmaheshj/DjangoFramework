5ed1c60364dd66ef6d86a035cc1fddaac6e356ae
Mahesh Jadhav <omkar28399@gmail.com>
2024-04-15 10:15:01 +0530

    Updated cache configuration to use file caching

--- d10_cache/d10_cache/settings.py
+++ d10_cache/d10_cache/settings.py
@@ -126,6 +126,15 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 ##################### Database Cache Configurations ##################
+# CASCHE_MIDDLEWARE_SECONDS = 30
+# CACHES = {
+#     'default':{
+#         'BACKEND':'django.core.cache.backends.db.DatabaseCache',
+#         'LOCATION':'cache_table'
+#     }
+# }
+
+##################### File Cache Configurations ##################
 CASCHE_MIDDLEWARE_SECONDS = 30
 CACHES = {
     'default':{
