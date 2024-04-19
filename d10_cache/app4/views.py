cb54272c13968bac634320e30e0dc36b5e511219
Mahesh Jadhav <omkar28399@gmail.com>
2024-04-19 09:57:07 +0530

    Added delete_many

--- d10_cache/app4/views.py
+++ d10_cache/app4/views.py
@@ -16,13 +16,11 @@ def lowlevelhome(request):
     kvs = {'Name':'Krishna','Age':0,'City':'Dwarka'}
     cache.set_many(kvs,60)
     data = cache.get_many(kvs)
-    cache.set('Name','Ram',version=2)
-    cache.set('Name','Vishnu',version=3)
-    cache.set('Name','Om',version=4)
     return render(request,'app4/home.html',{'data':data})
 
 def lowlevelgethome(request):
     keys = ['Name','Age']
     data = cache.get_many(keys) 
-    cache.delete('Name',version=2)
+    # cache.delete('Name',version=2)
+    cache.delete_many(['Name','Age','roll'])
     return render(request,'app4/home.html',{'data':data})
