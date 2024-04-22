2d438307a9e7072c8ca91baefc71c96d319f56cb
Mahesh Jadhav <omkar28399@gmail.com>
2024-04-22 22:35:16 +0530

    Added user_logged_out and user_login_failed signals

--- d11_signals/app1/signals.py
+++ d11_signals/app1/signals.py
@@ -12,5 +12,25 @@ def login_success(sender,request,user,**kwargs):
     print('User Password:',user.password)
     print('Kwargs',kwargs)
 
+@receiver(user_logged_out)
+def login_success(sender,request,user,**kwargs):
+    print('---------------------------------------')
+    print('------------Logged out Signal----------')
+    print('Sender:',sender)
+    print('Request:',request)
+    print('User:',user)
+    print('User Password:',user.password)
+    print('Kwargs',kwargs)
+
+@receiver(user_login_failed)
+def login_success(sender,credentials,request,**kwargs):
+    print('---------------------------------------')
+    print('----------- -Logged in Failed----------')
+    print('Sender:',sender)
+    print('Request:',request)
+    # print('User:',user)
+    print('credentials:',credentials)
+    print('Kwargs',kwargs)
+
 #Manual Signal Connection
 # user_logged_in.connect(login_success,sender=User,dispatch_uid='SuccessSignal')
