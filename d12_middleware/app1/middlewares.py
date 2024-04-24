293365746bf338889ea4cd44cb10c03fb6782175
Mahesh Jadhav <omkar28399@gmail.com>
2024-04-24 18:17:22 +0530

    Configured to send response from middleware instead going forward to view

--- d12_middleware/app1/middlewares.py
+++ d12_middleware/app1/middlewares.py
@@ -1,12 +1,13 @@
+from django.shortcuts import HttpResponse
+
+
 # Function Based Middleware
-from typing import Any
-
-
 def funcMiddleware(get_response):
     print('--------func:one-time configuration/initialization---------')
     def myMiddleware(request):
         print('-----func:Code execute before view-------')
-        response = get_response(request)
+        # response = get_response(request)
+        response = HttpResponse('Cant go forward, response from middleware')
         print('-----func:Code execute After view-------')
         return response
     return myMiddleware
