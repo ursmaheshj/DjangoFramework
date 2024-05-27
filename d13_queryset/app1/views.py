9b70ef5d5b1dd09d05e34bd0f77af4e9acb77eb4
Mahesh Jadhav <omkar28399@gmail.com>
2024-05-27 18:33:01 +0530

    added exclude method

--- d13_queryset/app1/views.py
+++ d13_queryset/app1/views.py
@@ -4,7 +4,9 @@ from app1.models import Student
 # Create your views here.
 def home(request):
     # data = Student.objects.all()
-    data = Student.objects.filter(marks = 99,name='Ram')
+    # data = Student.objects.filter(marks = 99,name='Ram')
+    data = Student.objects.exclude(marks = 99,name='Ram')
+    
     print("---Return Data:", data)
     print("---Query:",data.query)
     return render(request,'app1/home.html',{'data':data})
