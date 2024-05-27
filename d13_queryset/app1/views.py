432b702785b0bba47db050dd74cba64e8601bab8
Mahesh Jadhav <omkar28399@gmail.com>
2024-05-27 18:24:00 +0530

    Added filter method

--- d13_queryset/app1/views.py
+++ d13_queryset/app1/views.py
@@ -3,7 +3,8 @@ from app1.models import Student
 
 # Create your views here.
 def home(request):
-    data = Student.objects.all()
+    # data = Student.objects.all()
+    data = Student.objects.filter(marks = 99,name='Ram')
     print("---Return Data:", data)
     print("---Query:",data.query)
     return render(request,'app1/home.html',{'data':data})
