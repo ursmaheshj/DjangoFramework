0b38daa9461e7b31a36baf466396730e48990629
Mahesh Jadhav <omkar28399@gmail.com>
2024-05-27 18:18:25 +0530

    Updated Print

--- d13_queryset/app1/views.py
+++ d13_queryset/app1/views.py
@@ -4,7 +4,7 @@ from app1.models import Student
 # Create your views here.
 def home(request):
     data = Student.objects.all()
-    print(data)
-    print(data.query)
+    print("---Return Data:", data)
+    print("---Query:",data.query)
     return render(request,'app1/home.html',{'data':data})
     
