ba5451c08525706c525f9e1807e1f476ff1638a9
Mahesh Jadhav <omkar28399@gmail.com>
2024-05-27 18:55:35 +0530

    Added order_by methodo O

--- d13_queryset/app1/views.py
+++ d13_queryset/app1/views.py
@@ -5,8 +5,9 @@ from app1.models import Student
 def home(request):
     # data = Student.objects.all()
     # data = Student.objects.filter(marks = 99,name='Ram')
-    data = Student.objects.exclude(marks = 99,name='Ram')
-    
+    # data = Student.objects.exclude(marks = 99,name='Ram')
+    data = Student.objects.order_by('marks','-name')
+
     print("---Return Data:", data)
     print("---Query:",data.query)
     return render(request,'app1/home.html',{'data':data})
