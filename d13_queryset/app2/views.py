e8942847eace14e700e5b551e50dee30632b1138
Mahesh Jadhav <omkar28399@gmail.com>
2024-05-28 08:56:55 +0530

    added get method

--- d13_queryset/app2/views.py
+++ d13_queryset/app2/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from app1.models import Student,Teacher
 
 # Create your views here.
+def home2(request):
+    # data = Student.objects.get(pk=1)
+    data = Student.objects.get(name='Rutuja') # should be uniquely identifiable
+
+    print("---Return Data:", data)
+    # print("---Query:",data.query) #only used with queryset with list
+    return render(request,'app2/home2.html',{'data':data})
+    
--- d13_queryset/d13_queryset/urls.py
+++ d13_queryset/d13_queryset/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from app1.views import home
+from app2.views import home2
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
+    path('home/', home),
+    path('home2/', home2),
 ]
