7e17fd1b2884b282ce8636926d9f6aa2b8531351
Mahesh Jadhav <omkar28399@gmail.com>
2024-05-28 09:26:52 +0530

    added first method

--- d13_queryset/app2/views.py
+++ d13_queryset/app2/views.py
@@ -4,7 +4,9 @@ from app1.models import Student,Teacher
 # Create your views here.
 def home2(request):
     # data = Student.objects.get(pk=1)
-    data = Student.objects.get(name='Rutuja') # should be uniquely identifiable
+    # data = Student.objects.get(name='Rutuja') # should be uniquely identifiable
+    # data = Student.objects.first()
+    # data = Student.objects.order_by('name').first()
 
     print("---Return Data:", data)
     # print("---Query:",data.query) #only used with queryset with list
