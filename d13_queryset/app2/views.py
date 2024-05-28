d9aeda34cba215637d3c11f68b53651d2bb0c970
Mahesh Jadhav <omkar28399@gmail.com>
2024-05-28 10:19:25 +0530

    added create & get_or_create methods

--- d13_queryset/app2/views.py
+++ d13_queryset/app2/views.py
@@ -11,8 +11,11 @@ def home2(request):
     # data = Student.objects.latest('pass_date')
     # data = Student.objects.earliest('pass_date')
     # data = Student.objects.exists()
-    data = Student.objects.filter(name='Mahesh').exists()
-
+    # data = Student.objects.filter(name='Mahesh').exists()
+    # data=Student.objects.create(name='Radha',city='Mathura',roll=51,marks=100,pass_date='2020-3-28')
+    data,created=Student.objects.get_or_create(name='Radha',city='Mathura',roll=31,marks=100,pass_date='2020-3-28') #crated will be true false based on object creation
+    
+    
     print("---Return Data:", data)
     # print("---Query:",data.query) #only used with queryset with list
     return render(request,'app2/home2.html',{'data':data})
