3e1293aab2fc25841eeb4ebd820e62f8f122bb88
Mahesh Jadhav <omkar28399@gmail.com>
2024-05-31 00:18:32 +0530

    added exact lookup

--- d13_queryset/app3/views.py
+++ d13_queryset/app3/views.py
@@ -3,7 +3,10 @@ from app3.models import Student
 
 # Create your views here.
 def home3(request):
-    students = Student.objects.all()
+    # students = Student.objects.all()
+    # students = Student.objects.filter(name__exact='Rutuja')  #Case sensitive
+    students = Student.objects.filter(name__iexact='ruTuja')  #Case insensitive
+    
 
 
     print("---Return Data:", students)
