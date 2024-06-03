3c9eb015e1eb974a0f308da74bdeb93bec13dc49
Mahesh Jadhav <omkar28399@gmail.com>
2024-06-03 20:42:47 +0530

    Added model inheritance using base class in app1

--- d14_model_inheritance_relations/app1_inheritance/admin.py
+++ d14_model_inheritance_relations/app1_inheritance/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from app1_inheritance.models import CommonInfo,Student,Teacher,Contractor
 
 # Register your models here.
+# admin.site.register(CommonInfo)
+admin.site.register(Student)
+admin.site.register(Teacher)
+admin.site.register(Contractor)
--- d14_model_inheritance_relations/app1_inheritance/models.py
+++ d14_model_inheritance_relations/app1_inheritance/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class CommonInfo(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    date = models.DateField()
+    class Meta:        
+        abstract = True        #Makes this class abstract and wont show in admin dashboard
+
+class Student(CommonInfo):
+    fees = models.IntegerField()
+    date = None               #Wont inherit from parent
+
+class Teacher(CommonInfo):
+    salary = models.IntegerField()
+
+class Contractor(CommonInfo):
+    payment = models.IntegerField()
+    date = models.DateTimeField()   #override from parent
