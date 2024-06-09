1310061872ec8b2066edea901c9a4feee7334dbe
Mahesh Jadhav <omkar28399@gmail.com>
2024-06-09 22:04:34 +0530

    Updated code for proxy model

--- d14_model_inheritance_relations/app3_proxymodel/models.py
+++ d14_model_inheritance_relations/app3_proxymodel/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class ExamCenter(models.Model):
+    cname = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+
+class MyCenter(ExamCenter):
+    class Meta:
+        proxy = True
--- d14_model_inheritance_relations/d14_model_inheritance_relations/settings.py
+++ d14_model_inheritance_relations/d14_model_inheritance_relations/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'app1_inheritance',
     'app2_relation',
+    'app3_proxymodel',
 ]
 
 MIDDLEWARE = [
