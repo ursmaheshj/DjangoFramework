4020e61ff9cf298d895eeb9c86aeea6d45685b62
Mahesh Jadhav <omkar28399@gmail.com>
2024-06-11 18:42:39 +0530

    added many to many relationship between User and Post

--- d15_modelrelations/app2_manytoone/models.py
+++ d15_modelrelations/app2_manytoone/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=50,unique=True)
+    password = models.CharField(max_length=50)
+    def __str__(self):
+        return self.username
+    
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    publish = models.DateField()
+    def __str__(self):
+        return self.title
+    
--- d15_modelrelations/d15_modelrelations/settings.py
+++ d15_modelrelations/d15_modelrelations/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app1_onetoone',
+    'app2_manytoone',
 ]
 
 MIDDLEWARE = [
