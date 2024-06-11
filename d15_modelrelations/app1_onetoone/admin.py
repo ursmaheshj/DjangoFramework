3c3f0f195033d334abe183d08cc7c2038bba03b6
Mahesh Jadhav <omkar28399@gmail.com>
2024-06-11 10:09:01 +0530

    added one to one relatioship between husband and wife model

--- d15_modelrelations/app1_onetoone/admin.py
+++ d15_modelrelations/app1_onetoone/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from app1_onetoone.models import Husband,Wife
 
 # Register your models here.
+admin.site.register(Husband)
+admin.site.register(Wife)
--- d15_modelrelations/app1_onetoone/models.py
+++ d15_modelrelations/app1_onetoone/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Husband(models.Model):
+    name = models.CharField(max_length=50)
+    salary = models.IntegerField()
+    def __str__(self):
+        return self.name
+    
+
+class Wife(models.Model):
+    name = models.CharField(max_length=50)
+    husband = models.OneToOneField(Husband,on_delete=models.PROTECT)
+    def __str__(self):
+        return self.name
+    
