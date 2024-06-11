a2a29b17832b7df73aa9aba97402e77064426138
Mahesh Jadhav <omkar28399@gmail.com>
2024-06-11 11:20:16 +0530

    added model inheritance with onetoone relationship

--- d15_modelrelations/app1_onetoone/admin.py
+++ d15_modelrelations/app1_onetoone/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from app1_onetoone.models import Husband,Wife
+from app1_onetoone.models import Husband,Wife,UserModel,Page,Like
 
 # Register your models here.
 admin.site.register(Husband)
 admin.site.register(Wife)
+admin.site.register(UserModel)
+admin.site.register(Page)
+admin.site.register(Like)
--- d15_modelrelations/app1_onetoone/models.py
+++ d15_modelrelations/app1_onetoone/models.py
@@ -15,3 +15,27 @@ class Wife(models.Model):
     def __str__(self):
         return self.name
     
+
+
+#-------------Models to understand Model inheritance 
+class UserModel(models.Model):
+    name = models.CharField(max_length=50)
+    is_staff = models.BooleanField()
+    def __str__(self):
+        return self.name
+    
+
+class Page(models.Model):
+    name = models.CharField(max_length=50)
+    user = models.OneToOneField(UserModel,on_delete=models.CASCADE,primary_key=True)
+    publish = models.DateField()
+    def __str__(self):
+        return self.name
+    
+
+class Like(Page):
+    panna = models.OneToOneField(Page,on_delete=models.CASCADE,primary_key=True,parent_link=True)
+    likes = models.IntegerField()
+    def __str__(self):
+        return self.name
+    
