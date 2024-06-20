720bbdd4667d090b55ccf4272f7ae5afe221d39f
Mahesh Jadhav <omkar28399@gmail.com>
2024-06-20 21:44:50 +0530

    permanent=true redirects permanently

--- d16_classbasedviews/app3_redirectview/views.py
+++ d16_classbasedviews/app3_redirectview/views.py
@@ -4,3 +4,4 @@ from django.views.generic.base import TemplateView,RedirectView
 
 class MyRedirectView(RedirectView):
     pattern_name='mindex'
+    permanent = True #change status code from 301 to 302 indicating permanent redirect
