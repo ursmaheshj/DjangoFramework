4d68ca09a1d91c6cc524c41e075435f79399cf4f
Mahesh Jadhav <omkar28399@gmail.com>
2024-06-20 21:56:12 +0530

    added query_string and get_redirect_url method to deal with kwargs

--- d16_classbasedviews/app3_redirectview/views.py
+++ d16_classbasedviews/app3_redirectview/views.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.shortcuts import render
 from django.views.generic.base import TemplateView,RedirectView
 # Create your views here.
@@ -5,3 +6,10 @@ from django.views.generic.base import TemplateView,RedirectView
 class MyRedirectView(RedirectView):
     pattern_name='mindex'
     permanent = True #change status code from 301 to 302 indicating permanent redirect
+    query_string = True
+
+    def get_redirect_url(self, *args: Any, **kwargs: Any) -> str | None:
+        print(kwargs)
+        kwargs['pk']=45
+        return super().get_redirect_url(*args, **kwargs)
+    
