d87655dfe16f29b64ee61aead394ba593f8efc18
ursmaheshj <omkar28399@gmail.com>
2024-07-08 17:09:50 +0530

    Added ListView and template to display data

--- d17_genericviews/app1_listview/views.py
+++ d17_genericviews/app1_listview/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from app1_listview.models import Student
 
 # Create your views here.
+class StudentListView(ListView):
+    model = Student
+    # template_name = 'student.html'      #Custom template name
+    # context_object_name = 'students'    #Custom context name
--- d17_genericviews/d17_genericviews/urls.py
+++ d17_genericviews/d17_genericviews/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app1_listview.views import StudentListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    #ListView URLs
+    path('student/', StudentListView.as_view(),name='student'),
 ]
