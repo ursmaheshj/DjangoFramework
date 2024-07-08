0354ed8f53fa43acef96d6f969489805f9846280
ursmaheshj <mj241199@gmail.com>
2024-07-08 16:22:03 +0530

    Added model and included in adminsite

--- d17_genericviews/app1_listview/admin.py
+++ d17_genericviews/app1_listview/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from app1_listview.models import Student
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['id','name','roll','course']
