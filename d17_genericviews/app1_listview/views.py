6299e9df86ef05a8d924c4a6c067b4eb7683117a
ursmaheshj <omkar28399@gmail.com>
2024-07-08 17:30:00 +0530

    Added template suffix and ordering in d17/app1

--- d17_genericviews/app1_listview/views.py
+++ d17_genericviews/app1_listview/views.py
@@ -5,5 +5,7 @@ from app1_listview.models import Student
 # Create your views here.
 class StudentListView(ListView):
     model = Student
-    # template_name = 'student.html'      #Custom template name
+    # template_name_suffix = '_get'     #Custom suffix instead of _list use _get
+    # template_name = 'app1_listview/student.html'      #Custom template name
     # context_object_name = 'students'    #Custom context name
+    ordering = ['-name']
