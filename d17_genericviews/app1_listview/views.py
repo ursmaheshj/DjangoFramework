73d2072ac083cfcf88977d2ffa87cd82b2fd11fd
ursmaheshj <omkar28399@gmail.com>
2024-07-08 18:04:35 +0530

    added get_template_names to d17/app1 to render template based on cookie

--- d17_genericviews/app1_listview/views.py
+++ d17_genericviews/app1_listview/views.py
@@ -8,15 +8,24 @@ from app1_listview.models import Student
 class StudentListView(ListView):
     model = Student
     # template_name_suffix = '_get'     #Custom suffix instead of _list use _get
-    # template_name = 'app1_listview/student.html'      #Custom template name
+    template_name = 'app1_listview/student_list.html'      #Custom template name
     # context_object_name = 'students'    #Custom context name
     ordering = ['-name']
 
     def get_queryset(self) -> QuerySet[Any]:
         return Student.objects.filter(course='love')  #it will return only those students with course as love
     
-    def get_context_data(self, **kwargs) -> dict[str, Any]:
+    def get_context_data(self, **kwargs) -> dict[str, Any]:  #updates context variable along with new data
         context = super().get_context_data(**kwargs)
         context["freshers"] = Student.objects.all().order_by('name')
         return context
     
+    def get_template_names(self) -> list[str]:  #If cookie has user=ram it will render ram.html template
+        template_name = self.template_name
+        try:
+            if self.request.COOKIES['user']=='ram':
+                template_name = f'app1_listview/{self.request.COOKIES['user']}.html'
+        except Exception as e:
+            print(e)
+        
+        return [template_name]
