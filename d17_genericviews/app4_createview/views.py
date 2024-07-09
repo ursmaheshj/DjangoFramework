753e4d24bfec8e76602d0d9da7ed2b6c47bef58a
ursmaheshj <omkar28399@gmail.com>
2024-07-09 22:37:53 +0530

    add attrs like class to form fields

--- d17_genericviews/app4_createview/views.py
+++ d17_genericviews/app4_createview/views.py
@@ -1,14 +1,22 @@
+from django.forms import BaseModelForm
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from django.views.generic.detail import DetailView
 from app4_createview.models import Cstudent 
-
+from django import forms
 
 # Create your views here.
 class StudentCreateView(CreateView):
     model = Cstudent
     fields = ['name','roll','course']
     # success_url = '/createview/'
+    #to add class to form fields
+    def get_form(self, form_class: type[BaseModelForm] | None = ...) -> BaseModelForm:
+        form = super().get_form()
+        form.fields['name'].widget = forms.TextInput(attrs={'class':'myClass'})
+        form.fields['roll'].widget = forms.TextInput(attrs={'class':'myrollClass'})
+        form.fields['course'].widget = forms.TextInput(attrs={'class':'myClass'})
+        return form
     
 class StudentCreateDetailView(DetailView):
     model = Cstudent
