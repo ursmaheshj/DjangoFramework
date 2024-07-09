02ecd316f1a150ed6245522d9a457b68f4c5ea90
ursmaheshj <omkar28399@gmail.com>
2024-07-09 09:32:11 +0530

    added app3 for formview

--- d17_genericviews/app2_detailview/views.py
+++ d17_genericviews/app2_detailview/views.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from app2_detailview.models import Dstudent
@@ -5,5 +6,11 @@ from app2_detailview.models import Dstudent
 # Create your views here.
 class StudentDetailView(DetailView):
     model = Dstudent
-    template_name = 'app2_detailview/dstudent.html'
-    context_object_name = 'studentdetails'
+    # template_name = 'app2_detailview/dstudent.html' #Custom template name
+    # context_object_name = 'studentdetails'  #Custom context name
+    # pk_url_kwarg = 'id'  #can be used instead of pk in urls
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['all_student'] = self.model.objects.all()
+        return context
