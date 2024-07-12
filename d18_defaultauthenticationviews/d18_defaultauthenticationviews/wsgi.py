8c58eb485d74ce57435d7802454cf490428c55fc
ursmaheshj <omkar28399@gmail.com>
2024-07-12 10:16:26 +0530

    Added d18 to understand default authentication view provided by django

---
+++ d18_defaultauthenticationviews/d18_defaultauthenticationviews/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for d18_defaultauthenticationviews project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'd18_defaultauthenticationviews.settings')
+
+application = get_wsgi_application()
