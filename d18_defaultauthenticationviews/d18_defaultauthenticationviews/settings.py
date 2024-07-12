750756d38067f73e125a0460686683263aecfd26
ursmaheshj <omkar28399@gmail.com>
2024-07-12 10:29:48 +0530

    added template for login form

--- d18_defaultauthenticationviews/d18_defaultauthenticationviews/settings.py
+++ d18_defaultauthenticationviews/d18_defaultauthenticationviews/settings.py
@@ -52,10 +52,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'd18_defaultauthenticationviews.urls'
 
+TEMPLATES_DIR = BASE_DIR / 'templates'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
