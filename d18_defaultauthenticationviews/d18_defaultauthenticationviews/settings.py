cc754e2c25d6f93a6934bb0dfd5944920d34acce
ursmaheshj <omkar28399@gmail.com>
2024-07-16 22:41:14 +0530

    added email backend in settings

--- d18_defaultauthenticationviews/d18_defaultauthenticationviews/settings.py
+++ d18_defaultauthenticationviews/d18_defaultauthenticationviews/settings.py
@@ -123,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# EMAIL Backend
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
