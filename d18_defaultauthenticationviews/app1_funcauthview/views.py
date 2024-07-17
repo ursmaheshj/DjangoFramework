5d2bab3cabede79b0184442db9a71601bda44366
ursmaheshj <omkar28399@gmail.com>
2024-07-17 09:47:36 +0530

    added login_required decorator

--- d18_defaultauthenticationviews/app1_funcauthview/views.py
+++ d18_defaultauthenticationviews/app1_funcauthview/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required   #Makes sure user is logged in otherwise redirect to login
 def profile(request):
     return render(request,'registration/profile.html')
