04219e7c52fa44cb49cfeb79a841838d45ced800
ursmaheshj <omkar28399@gmail.com>
2024-07-19 18:41:38 +0530

    added user_passes_test decorator

--- d18_defaultauthenticationviews/app1_funcauthview/views.py
+++ d18_defaultauthenticationviews/app1_funcauthview/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required,permission_required
+from django.contrib.auth.decorators import login_required,permission_required,user_passes_test
 
 # Create your views here.
+
+def staff_member_required(user):
+    return user.is_staff
 # @login_required   #Makes sure user is logged in otherwise redirect to login
-@permission_required('app1_funcauthview.add_blog')
+# @permission_required('app1_funcauthview.add_blog')  #if user have given permission then only executes the view
+@user_passes_test(staff_member_required)
 def profile(request):
     return render(request,'registration/profile.html')
