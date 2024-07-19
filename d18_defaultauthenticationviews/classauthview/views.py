4d05e762867f76f76a93abc0e88bec59b5ef6689
ursmaheshj <omkar28399@gmail.com>
2024-07-19 20:51:25 +0530

    added shortcut for method decorator

--- d18_defaultauthenticationviews/classauthview/views.py
+++ d18_defaultauthenticationviews/classauthview/views.py
@@ -7,10 +7,15 @@ from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
 # Create your views here.
+# class Profile(TemplateView):
+#     template_name = 'registration/profile.html'
+
+#     @method_decorator(login_required)
+#     def dispatch(self, *args, **kwargs):
+#         return super().dispatch(*args, **kwargs)
+
+@method_decorator(login_required,name='dispatch')
 class Profile(TemplateView):
     template_name = 'registration/profile.html'
 
-    @method_decorator(login_required)
-    def dispatch(self, *args, **kwargs):
-        return super().dispatch(*args, **kwargs)
-        
+    
