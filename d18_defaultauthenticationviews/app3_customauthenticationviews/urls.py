9fbc98d741bc154b048ad95ead01fcd358158819
ursmaheshj <omkar28399@gmail.com>
2024-07-29 19:44:54 +0530

    Inherited Default view and customized

--- d18_defaultauthenticationviews/app3_customauthenticationviews/urls.py
+++ d18_defaultauthenticationviews/app3_customauthenticationviews/urls.py
@@ -17,12 +17,14 @@ Including another URLconf
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from django.views.generic import TemplateView
-from app3_customauthenticationviews.forms import LoginForm
+# from app3_customauthenticationviews.forms import LoginForm
+from app3_customauthenticationviews import views as myauth_views
 
 urlpatterns = [
  path('',TemplateView.as_view(template_name='myapp/home.html'),name='home'),
  path('dashboard/',TemplateView.as_view(template_name='myapp/dashboard.html'),name='dashboard'),
- path('login/',auth_views.LoginView.as_view(template_name='myapp/login.html',form_class=LoginForm),name='login'),
+#  path('login/',auth_views.LoginView.as_view(template_name='myapp/login.html',form_class=LoginForm),name='login'),
+ path('login/',myauth_views.MyLoginView.as_view(),name='login'),
  path('logout/',auth_views.LogoutView.as_view(template_name='myapp/logout.html'),name='logout'),
  path('changepass/',auth_views.PasswordChangeView.as_view(template_name='myapp/changepass.html',success_url='/myapp/changepassdone/'),name='changepass'),
  path('changepassdone/',auth_views.PasswordChangeDoneView.as_view(template_name='myapp/changepassdone.html'),name='changepassdone'),
--- d18_defaultauthenticationviews/app3_customauthenticationviews/views.py
+++ d18_defaultauthenticationviews/app3_customauthenticationviews/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from app3_customauthenticationviews.forms import LoginForm
+
 
 # Create your views here.
+class MyLoginView(LoginView):
+    template_name='myapp/login.html'
+    form_class=LoginForm
