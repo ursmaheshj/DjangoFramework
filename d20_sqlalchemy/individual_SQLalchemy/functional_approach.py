b33fb28b249c7ffaa162f01dc6f19f24511e312a
ursmaheshj <omkar28399@gmail.com>
2024-08-08 10:36:00 +0530

    Added simple functional approach to use sqlalchemy with inmemory sqlite db

---
+++ d20_sqlalchemy/individual_SQLalchemy/functional_approach.py
@@ -0,0 +1,38 @@
+import sqlalchemy as sa
+
+engine = sa.create_engine("sqlite:///:memory:")
+connection = engine.connect()
+
+metadata = sa.MetaData()
+
+user_table = sa.Table(
+    "user",
+    metadata,
+    sa.Column('id',sa.Integer,primary_key=True),
+    sa.Column("username",sa.String),
+    sa.Column("email",sa.String),
+)
+
+def insert_user(username:str,email:str)->None:
+    query = user_table.insert().values(username=username,email=email)
+    connection.execute(query)
+
+def select_user(username:str)->sa.engine.Result:
+    query = user_table.select().where(user_table.c.username==username)
+    result = connection.execute(query)
+    return result.fetchall()
+def worked():
+    print("Worked")
+
+def main()->None:
+    metadata.create_all(engine)
+    insert_user("Mahesh","mahesh@gmail.com")
+    insert_user("Ram","ram@gmail.com")
+    insert_user("Mahesh","mj@gmail.com")
+    print(select_user("Mahesh"))
+    worked()
+    connection.close()
+
+if __name__ == '__main__':
+    main()
+
