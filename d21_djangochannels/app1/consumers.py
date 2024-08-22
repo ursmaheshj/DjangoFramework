5563a358a73ae9c9d641b4e22fc3f3d008f5391a
ursmaheshj <omkar28399@gmail.com>
2024-08-22 21:30:33 +0530

    added routing and updated consumer code

--- d21_djangochannels/app1/consumers.py
+++ d21_djangochannels/app1/consumers.py
@@ -1,17 +1,50 @@
 from channels.consumer import SyncConsumer,AsyncConsumer
+from channels.exceptions import StopConsumer 
+
 
 class MySyncConsumer(SyncConsumer):
     def websocket_connect(self,event):
-        print('---Websocket connected---')
+        print('---Websocket connected---',event)
+        self.send({
+            'type':'websocket.accept'
+        })
     def websocket_receive(self,event):
-        print('---Websocket received---')
+        print('---Websocket received---',event)
+        print('--Message received--:',event['text'])
     def websocket_disconnect(self,event):
-        print('---Websocket disconnected---')
+        print('---Websocket disconnected---',event)
+        raise StopConsumer #it wont show unnecessary [took too long to shut down and was killed.] everytime
 
 class MyAsyncConsumer(AsyncConsumer):
     async def websocket_connect(self,event):
-        print('---Websocket connected---')
+        print('---Websocket connected---',event)
+        await self.send({
+            'type':'websocket.accept'
+        })
     async def websocket_receive(self,event):
-        print('---Websocket received---')
+        print('---Websocket received---',event)
+        print('--Message received--:',event['text'])
     async def websocket_disconnect(self,event):
-        print('---Websocket disconnected---')
+        print('---Websocket disconnected---',event)
+        raise StopConsumer
+
+
+
+# import json
+# from channels.generic.websocket import WebsocketConsumer
+# class ChatConsumer(WebsocketConsumer):
+
+#     def connect(self):
+#         self.username = "Anonymous"
+#         self.accept()
+#         self.send(text_data="[Welcome %s!]" % self.username)
+
+#     def receive(self, *, text_data):
+#         if text_data.startswith("/name"):
+#             self.username = text_data[5:].strip()
+#             self.send(text_data="[set your username to %s]" % self.username)
+#         else:
+#             self.send(text_data=self.username + ": " + text_data)
+
+#     def disconnect(self, message):
+#         pass
--- d21_djangochannels/d21_djangochannels/asgi.py
+++ d21_djangochannels/d21_djangochannels/asgi.py
@@ -10,10 +10,12 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter,URLRouter
+from app1.routing import ws_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'd21_djangochannels.settings')
 
 application = ProtocolTypeRouter({
-    'http':get_asgi_application,
+    'http':get_asgi_application(),
+    'websocket': URLRouter(ws_urlpatterns)
 })
