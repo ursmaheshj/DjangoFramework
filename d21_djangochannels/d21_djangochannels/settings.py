11ae83433dd8da6ed0dd0d1a3c431aabca90a9b9
ursmaheshj <omkar28399@gmail.com>
2024-08-26 22:57:47 +0530

    added django channels layer configuration in settings

--- d21_djangochannels/d21_djangochannels/settings.py
+++ d21_djangochannels/d21_djangochannels/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app1',
+    'app2',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Django Channels Configuration
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
